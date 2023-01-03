e7e0fefdc82bfd16a2e4ce0e4d130024d697aad5
JonatanSitompul <117884416+JonatanSitompul@users.noreply.github.com>
2023-01-03 12:18:28 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,24 @@
+print ('******Kredit Keaktifan Mahasiswa******2')
+print ('1. Menambahkan kegiatan')
+print ('2. Menampilkan Jumlah Poin')
+print ('3. Keluar')
+print ('---------------------------')
+
+while True:
+    # take input from the user
+    choice = input('Silakan masukan pilihan anda: ')
+
+    if choice == '1':
+        a = input('Nama Kegiatan: ')
+        b = input('Tanggal Kegiatan: ')
+        print('Pilihan Kategori Kegiatan')
+        print(' - Prestasi')
+        print(' - Organisasi')
+        print(' - Kepanitiaan')
+        print(' - Rekognisi')
+        c = input('Masukkan Kategori Kegiatan: ')
+        else 
+        print('Kegiatan berhasil ditambahkan')
+            
+
+
---
+++ soal2.py
@@ -0,0 +1,7 @@
+import turtle
+ 
+turtle.color("blue")
+turtle.penup()
+turtle.goto(-110, -25)
+turtle.pendown()
+turtle.circle(45)
